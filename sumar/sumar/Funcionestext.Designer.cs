fa0db1008bb7c7159bbe430f9f3e9ef04c384aae
KChaver <kevinchaver14@gmail.com>
2022-02-01 22:32:24 -0600

    funciones de texto
    ejercicio en el que se puede ver la cadena y sus funciones

---
+++ sumar/sumar/Funcionestext.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace sumar
+{
+    partial class Funcionestext
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CadenaTextBox = new System.Windows.Forms.TextBox();
+            this.ProcesarButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.LongitudTextBox = new System.Windows.Forms.TextBox();
+            this.PrimerCaracterTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.UltimoCaracterTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.RangoTextBox = new System.Windows.Forms.TextBox();
+            this.MayusculaTextBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.MinusculaTextBox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.RemplazarTextBox = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // CadenaTextBox
+            // 
+            this.CadenaTextBox.Location = new System.Drawing.Point(167, 12);
+            this.CadenaTextBox.Name = "CadenaTextBox";
+            this.CadenaTextBox.Size = new System.Drawing.Size(527, 23);
+            this.CadenaTextBox.TabIndex = 0;
+            // 
+            // ProcesarButton
+            // 
+            this.ProcesarButton.Location = new System.Drawing.Point(619, 51);
+            this.ProcesarButton.Name = "ProcesarButton";
+            this.ProcesarButton.Size = new System.Drawing.Size(75, 23);
+            this.ProcesarButton.TabIndex = 1;
+            this.ProcesarButton.Text = "Procesar";
+            this.ProcesarButton.UseVisualStyleBackColor = true;
+            this.ProcesarButton.Click += new System.EventHandler(this.ProcesarButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(103, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Longitud";
+            // 
+            // LongitudTextBox
+            // 
+            this.LongitudTextBox.Location = new System.Drawing.Point(167, 74);
+            this.LongitudTextBox.Name = "LongitudTextBox";
+            this.LongitudTextBox.Size = new System.Drawing.Size(100, 23);
+            this.LongitudTextBox.TabIndex = 3;
+            // 
+            // PrimerCaracterTextBox
+            // 
+            this.PrimerCaracterTextBox.Location = new System.Drawing.Point(167, 123);
+            this.PrimerCaracterTextBox.Name = "PrimerCaracterTextBox";
+            this.PrimerCaracterTextBox.Size = new System.Drawing.Size(100, 23);
+            this.PrimerCaracterTextBox.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(69, 131);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Primer Caracter";
+            // 
+            // UltimoCaracterTextBox
+            // 
+            this.UltimoCaracterTextBox.Location = new System.Drawing.Point(167, 171);
+            this.UltimoCaracterTextBox.Name = "UltimoCaracterTextBox";
+            this.UltimoCaracterTextBox.Size = new System.Drawing.Size(100, 23);
+            this.UltimoCaracterTextBox.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(68, 179);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Ultimo Caracter";
+            // 
+            // RangoTextBox
+            // 
+            this.RangoTextBox.Location = new System.Drawing.Point(167, 217);
+            this.RangoTextBox.Name = "RangoTextBox";
+            this.RangoTextBox.Size = new System.Drawing.Size(100, 23);
+            this.RangoTextBox.TabIndex = 9;
+            // 
+            // MayusculaTextBox
+            // 
+            this.MayusculaTextBox.Location = new System.Drawing.Point(167, 259);
+            this.MayusculaTextBox.Name = "MayusculaTextBox";
+            this.MayusculaTextBox.Size = new System.Drawing.Size(100, 23);
+            this.MayusculaTextBox.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(94, 267);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 15);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Mayuscula";
+            // 
+            // MinusculaTextBox
+            // 
+            this.MinusculaTextBox.Location = new System.Drawing.Point(167, 301);
+            this.MinusculaTextBox.Name = "MinusculaTextBox";
+            this.MinusculaTextBox.Size = new System.Drawing.Size(100, 23);
+            this.MinusculaTextBox.TabIndex = 13;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(96, 309);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(62, 15);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Minuscula";
+            // 
+            // RemplazarTextBox
+            // 
+            this.RemplazarTextBox.Location = new System.Drawing.Point(167, 346);
+            this.RemplazarTextBox.Name = "RemplazarTextBox";
+            this.RemplazarTextBox.Size = new System.Drawing.Size(100, 23);
+            this.RemplazarTextBox.TabIndex = 15;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(96, 354);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(62, 15);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Remplazar";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(103, 225);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 15);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Rango";
+            // 
+            // Funcionestext
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.RemplazarTextBox);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.MinusculaTextBox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.MayusculaTextBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.RangoTextBox);
+            this.Controls.Add(this.UltimoCaracterTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.PrimerCaracterTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.LongitudTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ProcesarButton);
+            this.Controls.Add(this.CadenaTextBox);
+            this.Name = "Funcionestext";
+            this.Text = "Funcionestext";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox CadenaTextBox;
+        private Button ProcesarButton;
+        private Label label1;
+        private TextBox LongitudTextBox;
+        private TextBox PrimerCaracterTextBox;
+        private Label label2;
+        private TextBox UltimoCaracterTextBox;
+        private Label label3;
+        private TextBox RangoTextBox;
+        private TextBox MayusculaTextBox;
+        private Label label5;
+        private TextBox MinusculaTextBox;
+        private Label label6;
+        private TextBox RemplazarTextBox;
+        private Label label7;
+        private Label label4;
+    }
+}
---
+++ sumar/sumar/Funcionestext.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sumar
+{
+    public partial class Funcionestext : Form
+    {
+        public Funcionestext()
+        {
+            InitializeComponent();
+        }
+
+        private void ProcesarButton_Click(object sender, EventArgs e)
+        {
+            String cadena = CadenaTextBox.Text;
+
+            LongitudTextBox.Text = cadena.Length.ToString();
+
+            PrimerCaracterTextBox.Text = cadena.Substring(0, 1);
+
+            UltimoCaracterTextBox.Text = cadena.Substring(cadena.Length - 1, 1);
+
+            RangoTextBox.Text = cadena.Substring(12, 20);
+
+            MayusculaTextBox.Text = cadena.ToUpper();
+
+            MinusculaTextBox.Text = cadena.ToLower();
+
+            RemplazarTextBox.Text = cadena.Replace(" ", "");
+
+
+
+        }
+    }
+}
